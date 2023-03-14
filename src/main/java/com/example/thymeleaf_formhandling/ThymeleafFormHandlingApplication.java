cc7c19dd869cef45aedab141f6c8b99d06df2aa3
Teller501 <ande777j@stud.kea.dk>
2023-03-14 18:12:35 +0100

    initial commit

---
+++ src/main/java/com/example/thymeleaf_formhandling/ThymeleafFormHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_formhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafFormHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafFormHandlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thymeleaf_formhandling/ThymeleafFormHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_formhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafFormHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
